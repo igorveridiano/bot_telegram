51592ecd1ca7bc6d5ab8ebc628472669114ff7fe
Igor Veridiano Andrade <igorveridiano@hotmail.com>
2019-05-02 17:40:50 -0300

    Initial commit

---
+++ Codigo/bot.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater, Filters
+from telegram.ext import CommandHandler, MessageHandler
+
+updater = Updater(token="851095270:AAGYsUcw3zQ-iOmhPBE5A4EogpAXeBciZPA")
+dispatcher = updater.dispatcher
+
+def start(bot, update):
+
+    msg = "Welcome to Filmes Bot"
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=msg
+    )
+
+def echo(bot, update):
+
+    msg = "You type: " + update.message.text
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=msg
+    )
+
+start_handler = CommandHandler('start', start)
+echo_handler = MessageHandler(Filters.text, echo)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+
+updater.start_polling()
+updater.idle()
